f7b9c0465e8a6ec4bae96bed6298ddd1b3f40533
cailin0630 <903199067@qq.com>
2017-08-01 15:36:02 +0800

    import

---
+++ CapturerDemo/CapturerFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapturerDemo
+{
+    public class CapturerFactory
+    {
+        public Action<Bitmap> ImageCaptured;
+        private bool _running;
+        public void Start()
+        {
+            Task.Run(() =>
+            {
+                _running = true;
+                while (_running)
+                {
+                    var image = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                    var imgGraphics = Graphics.FromImage(image);
+                    imgGraphics.CopyFromScreen(0, 0, 0, 0,
+                        new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+                    ImageCaptured.Invoke(image);
+                    
+                }
+               
+            });
+
+        }
+
+        public void Stop()
+        {
+            _running = false;
+        }
+
+        public Bitmap GetSingleImage()
+        {
+            var image = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            var imgGraphics = Graphics.FromImage(image);
+            imgGraphics.CopyFromScreen(0, 0, 0, 0,
+                new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+            return image;
+        }
+    }
+}
---
+++ CapturerDemo/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CapturerDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CapturerFactory _capturer = new CapturerFactory();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase1_OnClick(object sender, RoutedEventArgs e)
+        {
+            _capturer.Start();
+            _capturer.ImageCaptured += bitmap =>
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Image.Source = ChangeBitmapToImageSource(bitmap);
+                    bitmap.Dispose();
+                }));
+            };
+        }
+
+        public ImageSource ChangeBitmapToImageSource(Bitmap bitmap)
+        {
+            var hBitmap = bitmap.GetHbitmap();
+            ImageSource wpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            if (!DeleteObject(hBitmap))
+            {
+                throw new System.ComponentModel.Win32Exception();
+            }
+            return wpfBitmap;
+        }
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern bool DeleteObject(IntPtr hObject);
+
+        private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
+        {
+            var bitmap = _capturer.GetSingleImage();
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "jpg|*.jpg*",
+                DefaultExt = ".jpg"
+            };
+            if (!saveFileDialog.ShowDialog() ?? false)
+                return;
+            var fileName = saveFileDialog.FileName;
+            bitmap.Save(fileName, ImageFormat.Jpeg);
+        }
+    }
+}
